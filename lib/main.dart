79be5f034aadb0c26c2042fef3944f24ccd58a97
Наиль Шарипов <nikorurk@icloud.com>
2022-09-07 10:14:21 +0500

    Polish unused code

--- lib/main.dart
+++ lib/main.dart
@@ -29,39 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // Using MultiProvider is convenient when providing multiple objects.
-//     return MultiProvider(
-//       providers: [
-//         // In here CatalogModel never changes, so a simple Provider
-//         // is sufficient.
-//         Provider(create: (context) => CatalogModel()),
-//         // CartModel is implemented as a ChangeNotifier, which calls for the use
-//         // of ChangeNotifierProvider. Moreover, CartModel depends
-//         // on CatalogModel, so a ProxyProvider is needed.
-//         ChangeNotifierProxyProvider<CatalogModel, CartModel>(
-//           create: (context) => CartModel(),
-//           update: (context, catalog, cart) {
-//             if (cart == null) throw ArgumentError.notNull('cart');
-//             cart.catalog = catalog;
-//             return cart;
-//           },
-//         ),
-//       ],
-//       child: MaterialApp(
-//         title: 'Provider Demo',
-//         theme: appTheme,
-//         initialRoute: '/',
-//         routes: {
-//           '/': (context) => const MyCatalog(),
-//           '/cart': (context) => const MyCart(),
-//         },
-//       ),
-//     );
-//   }
-// }
